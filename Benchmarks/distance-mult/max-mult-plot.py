10b3df05a963dc16f8c872b2dc78e77152099847
Kasper Nielsen <kasper0406@gmail.com>
2014-05-19 15:13:49 +0200

    Report updates

--- Benchmarks/distance-mult/max-mult-plot.py
+++ Benchmarks/distance-mult/max-mult-plot.py
@@ -21,7 +21,7 @@ plt.xkcd()
 
 # Min distance product base case plot
 plt.figure()
-plt.title("Varying base case size")
+plt.title("Minimum Distance Product running time")
 ax1 = plt.subplot(111)
 plt.xscale('log', basex=2)
 #plt.yscale('log', basey=2)
@@ -45,3 +45,30 @@ ax1.plot(slow_mult['N'], slow_mult['median'] / slow_mult['N'], label="Naive runn
 
 ax1.legend()
 plt.savefig('max-dist-mult.pdf')
+
+
+# Min distance product cpu intrinsics plot
+plt.figure()
+plt.title("Minimum Distance Product cpu intrinsics")
+ax1 = plt.subplot(111)
+plt.xscale('log', basex=2)
+#plt.yscale('log', basey=2)
+plt.ylabel('Normalized instruction count')
+plt.xlabel('N')
+plt.ticklabel_format(style='sci', axis='y', scilimits=(-1,2))
+
+tail_len = 20
+ax1.plot(mult_rand['N'].tail(tail_len), (mult_rand['instructions'] / mult_rand['N']).tail(tail_len), label="Random shuffle")
+ax1.plot(mult_short['N'].tail(tail_len), (mult_short['instructions'] / mult_short['N']).tail(tail_len), label="Short candidate list")
+ax1.plot(mult_long['N'].tail(tail_len), (mult_long['instructions'] / mult_long['N']).tail(tail_len), label="Long candidate list")
+
+#ax2 = ax1.twinx()
+#ax2.plot(mult_rand['N'].tail(tail_len), (mult_rand['L3_miss'] / mult_rand['N']).tail(tail_len), label="Random shuffle", ls="dashed")
+#ax2.plot(mult_short['N'].tail(tail_len), (mult_short['L3_miss'] / mult_short['N']).tail(tail_len), label="Short candidate list", ls="dashed")
+#ax2.plot(mult_long['N'].tail(tail_len), (mult_long['L3_miss'] / mult_long['N']).tail(tail_len), label="Long candidate list", ls="dashed")
+
+#ax2.set_yscale('log', basey=2)
+#ax2.plot(mult_long['N'], mult_long['instructions'] / mult_long['N'], label="Instructions", ls='dashed', color='c')
+
+ax1.legend()
+plt.savefig('max-dist-mult-cpu.pdf')
